c17ecba7a0140202b132e4132003a553ab7d1a04
BLANC ALEXIS p2110038 <alexis.blanc@etu.univ-lyon1.fr>
2023-03-11 23:56:16 +0100

    basket ok, need front

--- src/Controller/BasketsController.php
+++ src/Controller/BasketsController.php
@@ -1,18 +1,42 @@
 <?php
 
 namespace App\Controller;
+use App\Model\Entity\Card;
 
+/**
+ *
+ */
 class BasketsController  extends AppController
 {
+    /**
+     * @var \Cake\Datasource\RepositoryInterface|null
+     */
+    /**
+     * @var \Cake\Datasource\RepositoryInterface|null
+     */
+    private $Card;
+
+    /**
+     * @var \Cake\Datasource\RepositoryInterface|null
+     */
+
+
+    public function initialize(): void {
+        parent::initialize();
+        $this->card = $this->getTableLocator()->get('Cards');
+    }
+
     public function index(){
         $baskets=$this->request->getSession()->read('Basket');
         if($baskets){
+            $tabCards=array();
             foreach ($baskets as $basketID){
-                $cards=$this->Cards->get($basketID);
-                $this->set(compact('card'));
+                $cards=$this->card->get($basketID);
+                array_push($tabCards, $cards);
             }
-
+            $this->set(compact('tabCards'));
         }else{
+
             echo "Panier vide";
         }
     }
--- templates/Baskets/index.php
+++ templates/Baskets/index.php
@@ -1,14 +1,17 @@
 <div class="card col-lg pl-1">
     <div class="card-img-top img-thumbnail">
-        <?= $this->Html->image($card->urlPokemon, array('class' => 'img-fluid')) ?>
+        <?php foreach($tabCards as $cards): ?>
+
+        <?= $this->Html->image($cards->urlPokemon, array('class' => 'img-fluid')) ?>
     </div>
     <div class="card-body text-center">
         <div class="card-text">
             <!-- Product name-->
-            <h5 class="fw-bolder"><?php echo $card->namePokemon?></h5>
+            <h5 class="fw-bolder"><?php echo $cards->namePokemon?></h5>
 
-            <?php echo $card->prix."€" ?>
+            <?php echo $cards->prix."€" ?>
         </div>
     </div>
+    <?php endforeach; ?>
 
 </div>
