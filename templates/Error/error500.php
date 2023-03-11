c9767970bacaa8041364027adf3e3a59f5db7a2e
Alexis <bzdalexis@gmail.com>
2023-03-11 23:57:50 +0100

    add error 400 and 500

--- templates/Error/error500.php
+++ templates/Error/error500.php
@@ -1,45 +1,11 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \Cake\Database\StatementInterface $error
- * @var string $message
- * @var string $url
- */
-use Cake\Core\Configure;
-use Cake\Error\Debugger;
-
-$this->layout = 'error';
-
-if (Configure::read('debug')) :
-    $this->layout = 'dev_error';
-
-    $this->assign('title', $message);
-    $this->assign('templateName', 'error500.php');
-
-    $this->start('file');
-?>
-<?php if (!empty($error->queryString)) : ?>
-    <p class="notice">
-        <strong>SQL Query: </strong>
-        <?= h($error->queryString) ?>
-    </p>
-<?php endif; ?>
-<?php if (!empty($error->params)) : ?>
-    <strong>SQL Query Params: </strong>
-    <?php Debugger::dump($error->params) ?>
-<?php endif; ?>
-<?php if ($error instanceof Error) : ?>
-    <strong>Error in: </strong>
-    <?= sprintf('%s, line %s', str_replace(ROOT, 'ROOT', $error->getFile()), $error->getLine()) ?>
-<?php endif; ?>
-<?php
-    echo $this->element('auto_table_warning');
-
-    $this->end();
-endif;
-?>
-<h2><?= __d('cake', 'An Internal Error Has Occurred.') ?></h2>
-<p class="error">
-    <strong><?= __d('cake', 'Error') ?>: </strong>
-    <?= h($message) ?>
-</p>
+<div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center">
+                <img src="img/500.webp" alt="" width="500">
+                <h1 class="display-1 fw-bold">500</h1>
+                <p class="fs-3"> <span class="text-danger">Opps!</span> Server Error.</p>
+                <p class="lead">
+                    It's not you, it's me.
+                  </p>
+                <?php echo $this->Html->link('Go home', ['controller' => 'Cards', 'action' => 'index'], ['escapeTitle' => false, 'class' => 'btn btn-primary']);?>
+            </div>
+        </div>
