48cd614db259fe7eeada0a6abb3ae7ecb1342ebf
Alexis <bzdalexis@gmail.com>
2023-03-12 20:02:28 +0100

    .

--- templates/Error/error500.php
+++ templates/Error/error500.php
@@ -1,11 +1,11 @@
 <div class="d-flex align-items-center justify-content-center vh-100">
-            <div class="text-center">
-                <img src="img/500.webp" alt="" width="500">
-                <h1 class="display-1 fw-bold">500</h1>
-                <p class="fs-3"> <span class="text-danger">Opps!</span> Server Error.</p>
-                <p class="lead">
-                    It's not you, it's me.
-                  </p>
-                <?php echo $this->Html->link('Go home', ['controller' => 'Cards', 'action' => 'index'], ['escapeTitle' => false, 'class' => 'btn btn-primary']);?>
-            </div>
-        </div>
+    <div class="text-center">
+        <img src="img/500.webp" alt="" width="500">
+        <h1 class="display-1 fw-bold">500</h1>
+        <p class="fs-3"> <span class="text-danger">Opps!</span> Server Error.</p>
+        <p class="lead">
+            It's not you, it's me.
+        </p>
+        <?php echo $this->Html->link('Go home', ['controller' => 'Cards', 'action' => 'index'], ['escapeTitle' => false, 'class' => 'btn btn-primary']);?>
+    </div>
+</div>
