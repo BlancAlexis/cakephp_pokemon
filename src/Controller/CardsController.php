909190f27be16ff1fdeae4eeb55f21f7281243e8
BLANC ALEXIS p2110038 <alexis.blanc@etu.univ-lyon1.fr>
2023-03-12 10:40:42 +0100

    front basket and quantity

--- src/Controller/CardsController.php
+++ src/Controller/CardsController.php
@@ -10,18 +10,21 @@ class CardsController extends AppController {
          $this->set(compact('cards'));
     }
 
-    public function addToCart($cartID){
 
-    }
     public function click()
     {
         $basket = $this->request->getSession()->read('Basket') ?? [];
         $cardID = $this->request->getData('id');
-        array_push($basket,$cardID);
-        $this->request->getSession()->write('Basket', $basket);
+        if (in_array($cardID, $basket)) {
+            $key = array_search($cardID, $basket);
+            $basket[$key]++;
+        }
+        else {
+            array_push($basket, $cardID);
+            $this->request->getSession()->write('Basket', $basket);
+
+        }
         $this->redirect($this->referer());
     }
-    public function add(){
 
-    }
 }
--- templates/Baskets/index.php
+++ templates/Baskets/index.php
@@ -1,17 +1,43 @@
-<div class="card col-lg pl-1">
-    <div class="card-img-top img-thumbnail">
-        <?php foreach($tabCards as $cards): ?>
+<div class="container py-5">
 
-        <?= $this->Html->image($cards->urlPokemon, array('class' => 'img-fluid')) ?>
-    </div>
-    <div class="card-body text-center">
-        <div class="card-text">
-            <!-- Product name-->
-            <h5 class="fw-bolder"><?php echo $cards->namePokemon?></h5>
+    <!-- For demo purpose -->
 
-            <?php echo $cards->prix."€" ?>
+    <!-- End -->
+
+    <div class="row">
+        <div class="col-lg-8 mx-auto">
+
+            <!-- List group-->
+            <ul class="list-group shadow">
+                <?php foreach($tabCards as $cards): ?>
+                <!-- list group item-->
+                <li class="list-group-item">
+                    <!-- Custom content-->
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                        <div class="media-body order-2 order-lg-1">
+                            <h5 class="mt-0 font-weight-bold mb-2"><?php echo $cards->namePokemon?></h5>
+                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                <h6 class="font-weight-bold my-2"><?php echo $cards->prix."€" ?></h6>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                <h6 class="font-weight-bold my-2"><?php echo "Stock : ".$cards->stock?></h6>
+                            </div>
+                           <!-- <div class="d-flex align-items-center justify-content-between mt-1">
+                                <h6 class="font-weight-bold my-2"><?php /*echo "Quantité demandé : ".$basket[1]++;*/?></h6>
+                            </div>-->
+                        </div>
+                    </div>
+                        <div class="col-lg-4 mx-auto">
+                        <?= $this->Html->image($cards->urlPokemon, array('class' => 'img-fluid', 'width'=>'200px')) ?>
+                    </div>
+
+                    <!-- End -->
+                </li>
+                <!-- End -->
+                <?php endforeach; ?>
+
+            </ul>
+            <!-- End -->
         </div>
     </div>
-    <?php endforeach; ?>
-
 </div>
