e986ad895e411c2d59d2204e4da036724e385bec
BLANC ALEXIS p2110038 <alexis.blanc@etu.univ-lyon1.fr>
2023-03-12 14:23:30 +0100

    basket ok

--- src/Controller/CardsController.php
+++ src/Controller/CardsController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-class CardsController extends AppController {
+class CardsController extends AppController
+{
 
-    public function index(){
-         $this->loadComponent('Paginator');
-         $cards=$this->Paginator->paginate($this->Cards->find());
-         $this->set(compact('cards'));
+    public function index()
+    {
+        $this->loadComponent('Paginator');
+        $cards = $this->Paginator->paginate($this->Cards->find());
+        $this->set(compact('cards'));
     }
 
 
@@ -18,13 +20,11 @@ class CardsController extends AppController {
         if (in_array($cardID, $basket)) {
             $key = array_search($cardID, $basket);
             $basket[$key]++;
-        }
-        else {
+        } else {
             array_push($basket, $cardID);
             $this->request->getSession()->write('Basket', $basket);
 
         }
         $this->redirect($this->referer());
     }
-
 }
--- templates/Baskets/index.php
+++ templates/Baskets/index.php
@@ -1,44 +1,45 @@
-<div class="container py-5">
-
-    <!-- For demo purpose -->
-
-    <!-- End -->
+<?php $indice = 0;
+$somme=0;
+foreach ($tabCards as $cards): ?>
+<div class="container py-5 ">
 
     <div class="row">
         <div class="col-lg-8 mx-auto">
 
             <!-- List group-->
             <ul class="list-group shadow">
-                <?php $indice=0;
-                 foreach($tabCards as $cards): ?>
+
                 <!-- list group item-->
                 <li class="list-group-item">
                     <!-- Custom content-->
                     <div class="media align-items-lg-center flex-column flex-lg-row p-3">
-                        <div class="media-body order-2 order-lg-1">
-                            <h5 class="mt-0 font-weight-bold mb-2"><?php echo $cards->namePokemon?></h5>
+                        <div class="media-body order-2 order-lg-1 media align-items-center">
+                            <h5 class="mt-0 font-weight-bold mb-2"><?php echo $cards->namePokemon ?></h5>
                             <div class="d-flex align-items-center justify-content-between mt-1">
-                                <h6 class="font-weight-bold my-2"><?php echo $cards->prix."€" ?></h6>
+                                <h6 class="font-weight-bold my-2"><?php echo $cards->prix . "€" ?></h6>
                             </div>
                             <div class="d-flex align-items-center justify-content-between mt-1">
-                                <h6 class="font-weight-bold my-2"><?php echo "Stock : ".$cards->stock?></h6>
+                                <h6 class="font-weight-bold my-2"><?php echo "Stock : " . $cards->stock ?></h6>
                             </div>
                             <div class="d-flex align-items-center justify-content-between mt-1">
-                                <h6 class="font-weight-bold my-2"><?php echo print_r($baskets);"Quantité demandé : ".($baskets[$indice]);?></h6>
+                                <h6 class="font-weight-bold my-2"><?php echo "Quantité demandé : " . ($baskets[$indice]); ?></h6>
                             </div>
                         </div>
-                    </div>
-                        <div class="col-lg-4 mx-auto">
-                        <?= $this->Html->image($cards->urlPokemon, array('class' => 'img-fluid', 'width'=>'200px')) ?>
-                    </div>
-               <?= $indice++?>
-                    <!-- End -->
                 </li>
-                <!-- End -->
-                <?php endforeach; ?>
-
             </ul>
-            <!-- End -->
+        </div>
+        <div class="col-lg-4 mx-auto">
+            <?= $this->Html->image($cards->urlPokemon, array('class' => 'img-fluid', 'width' => '200px')) ?>
         </div>
     </div>
+
+</div>
+</div>
+
+<?php $indice++;
+$somme+=$cards->prix;
+endforeach;
+?>
+<div class="col-lg-2 mx-auto">
+<?php echo "Pour un total de ".$somme."€"?>
 </div>
